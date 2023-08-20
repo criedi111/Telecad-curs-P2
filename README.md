c710f6ff8af6cf80d5a32623e6e74a345acb681c
EDUARD <eduardfile111111@gmail.com>
2023-08-20 18:28:18 +0300

    full-project-commit

---
+++ README.md
@@ -0,0 +1,11 @@
+## JavaScript Advanced - Curs 3
+
+Pentru a rula aceasta etapa (in linie de comanda):
+
+#### 1. `git clone https://github.com/razvancir96/react-curs03.git`
+
+#### 2. `cd react-curs03`
+
+#### 3. `npm install` (posibil sa dureze)
+
+#### 4. `npm start`
---
+++ src/components/UserItem.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function UserItem(props) {
+    // Adaugam un prop nou: isGoldClient - daca e true => afisam mesajul "Client GOLD",
+    // altfel nu afisam nimic.
+    const {name, email, isGoldClient} = props;
+    return (
+        <div>
+            <h2>{ name }</h2>
+            <p>{ email }</p>
+            {/* Pentru a pune o conditie in afisarea elementelor, folosim operatorul ternar. */}
+            {/* Pentru sintaxa, revizuiti subcapitolul "Operatorul ternar" de la
+            cursul 3 - Randare conditionata.*/}
+            {/* ATENTIE! Daca pe ramura de else nu avem nimic, punem null.*/}
+            { isGoldClient // Daca isGoldClient are valoare truthy, intra pe prima ramura (dupa ?).
+                ? <h3>Client GOLD</h3>
+                : null
+            }
+            {/* PRO Tip: O varinata mai eleganta aici ar fi fost utilizarea unui SHORTHAND
+            CIRCUIT (opratorii && sau ||). Topicul e usor mai avansat, dar curiosii pot da un
+            search pe Google :). */}
+        </div>
+    );
+}
+
+export default UserItem;
